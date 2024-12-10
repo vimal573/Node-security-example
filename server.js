243d645276dbfd5eec8266caae8d62d3501f27d1
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-12-10 18:44:53 +0530

    Setting Up OAuth Cookie Session

--- server.js
+++ server.js
@@ -5,15 +5,23 @@ const express = require('express');
 const helmet = require('helmet');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const cookieSession = require('cookie-session');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
+};
+
 const AUTH_OPTIONS = {
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
   callbackURL: '/auth/google/callback',
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
 };
 
 function verifyCallback(accessToken, refershToken, profile, done) {
@@ -24,16 +32,17 @@ function verifyCallback(accessToken, refershToken, profile, done) {
 
 passport.use(new GoogleStrategy(AUTH_OPTIONS, verifyCallback));
 
-// const config = {
-//   CLIENT_ID: process.env.CLIENT_ID,
-//   CLIENT_SECRET: process.env.CLIENT_SECRET,
-// };
-
-// passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
-
 const app = express();
 
 app.use(helmet());
+
+app.use(
+  cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2], // 'secret key'
+  })
+);
 app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
